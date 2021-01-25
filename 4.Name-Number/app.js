e34d22d707bdb179efc71d1f6929ed9eaa3c97e5
unknown <0918.mohamad@gmail.com>
2021-01-25 13:48:01 +0330

    ===== 4.Name-Numbers ===== fix numbers object change merge function add a function to print result in a file (resut.txt) add files folder to store input and output files

--- 4.Name-Number/app.js
+++ 4.Name-Number/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const message = {
+const FILE_PATH = __dirname+'/files/';
+const MESSAGE = {
     zero: "hasn't any phone number.",
     one: "'s phone number is ",
     many: "'s phone numbers are "
@@ -10,32 +11,32 @@ const makeArray = arr => {
         if(!value) return;
         let [key, val] = value.split('-').map(item=>item.trim());
         // if key is defined [add] new value | dont replace value
-        if(result.hasOwnProperty(key))
-            result[key] = [result[key],val];
-        else
-            result[key] = val;
+        if(result.hasOwnProperty(key)) result[key] = [...result[key],val];
+        else result[key] = [val];
     });
     return result;
 }
 const objectFind = (obj, targetKey) => {
-    let result = [];
-    for (const [key, value] of Object.entries(obj)) {
-        if(key === targetKey) result.push(value);
-    }
-    return result;
+    for (const [key, value] of Object.entries(obj))
+        if(key === targetKey) return value;
+   return [];
 }
-const printNamesNumber = (names, numbers) => {
-    for (const [index, value] of Object.entries(names)) {
-        if(names.hasOwnProperty(index)){
-            let num = objectFind(numbers, index);
-            if(num.length === 0) console.log(`${value} ${message.zero}`);
-            else if (typeof num[0] === 'string') console.log(`${value}${message.one}${num[0]}`);
-            else console.log(`${value}${message.many}${num.join(',')}`);
-        }
+const printResult = data => {
+    console.log(data);
+    fs.appendFileSync(FILE_PATH+"result.txt",data+"\n");
+}
+const mergeNamesNumbers = (names, numbers) => {
+    // delete last data
+    fs.writeFileSync(FILE_PATH+"result.txt","");
+    for (const [key, value] of Object.entries(names)) {
+        let num = objectFind(numbers, key);
+        if(num.length === 0) printResult(`${value} ${MESSAGE.zero}`);
+        else if (num.length === 1) printResult(`${value}${MESSAGE.one}${num[0]}`);
+        else printResult(`${value}${MESSAGE.many}${num.join(',')}`);
     }
 }
 
-let names = makeArray(fs.readFileSync(__dirname+'/names.txt',"utf-8").split("\r\n"));
-let numbers = makeArray(fs.readFileSync(__dirname+'/numbers.txt',"utf-8").split("\r\n"));
+let names = makeArray(fs.readFileSync(FILE_PATH+'names.txt',"utf-8").split("\r\n"));
+let numbers = makeArray(fs.readFileSync(FILE_PATH+'numbers.txt',"utf-8").split("\r\n"));
 
-printNamesNumber(names, numbers);
+mergeNamesNumbers(names, numbers);
